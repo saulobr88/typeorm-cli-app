82e93bedec9d2aea8329f26a067ad8abc9f8bc3e
Saulo Gomes <saulo.sgs@ufpe.br>
2025-06-17 10:07:59 -0300

    Added suppor for SQLite3 DB

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,20 +1,39 @@
 // src/database/data-source.ts
 import 'reflect-metadata';
-import { DataSource } from 'typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import path from 'path';
 import dotenv from 'dotenv';
 
 dotenv.config({ path: path.join(process.cwd(), '.env') });
 
-export const AppDataSource = new DataSource({
+
+const dbDriver = (process.env.DB_DRIVER || '').toLowerCase() || 'postgres';
+
+const dataSourceObjEntities = {
+  entities:   [path.join(__dirname, '../entities/*.ts')],
+  migrations: [path.join(__dirname, './migrations/*.ts')],
+  synchronize: false,             // nunca em produção!
+}
+
+let dataSourceObj: DataSourceOptions;
+
+dataSourceObj = {
   type:     'postgres',
   host:     process.env.DB_HOST,
   port:     Number(process.env.DB_PORT),
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ...dataSourceObjEntities,
+};
 
-  entities:   [path.join(__dirname, '../entities/*.ts')],
-  migrations: [path.join(__dirname, './migrations/*.ts')],
-  synchronize: false,             // nunca em produção!
-});
+if (dbDriver === 'sqlite' || dbDriver === 'better-sqlite3') {
+  dataSourceObj = {
+    type: 'better-sqlite3',
+    database: process.env.SQLITE_PATH || './.data/db.sqlite',
+    ...dataSourceObjEntities,
+    migrations: [path.join(__dirname, './migrations/sqlite3/*.ts')],
+  };
+}
+
+export const AppDataSource = new DataSource(dataSourceObj);
